732656d0c00bb72eccc5acc719f256ca1ab33c99
Vladimír Slavík <elviin@gmail.com>
2020-06-30 09:27:25 +0200

    [UPD] added the swift project

---
+++ TMDB-Swift/TMDB-SwiftTests/TMDB_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  TMDB_SwiftTests.swift
+//  TMDB-SwiftTests
+//
+//  Created by Vladimir Slavik on 29.06.20.
+//  Copyright © 2020 Vladimir Slavik. All rights reserved.
+//
+
+import XCTest
+@testable import TMDB_Swift
+
+class TMDB_SwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
